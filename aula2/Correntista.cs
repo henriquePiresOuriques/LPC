9280c9a1c5ad9e47a55e9a9236e93b1f99018d14
henriquePiresOuriques <henriquepiresouriques@gmail.com>
2019-08-12 16:02:58 -0300

    add aula2/

---
+++ aula2/Correntista.cs
@@ -0,0 +1,12 @@
+namespace aula2
+{
+    public class Correntista
+    {
+        public Correntista (int codigo, string nome){
+            this.codigo = codigo;
+            this.nome = nome;
+        }
+        public int codigo { get; private set; }
+        public string nome {get; private set;}
+    }
+}
---
+++ aula2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace aula2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Conta objContaDoJoao = 
+                new Conta(1234,
+                      new Correntista(121, "joão da silva"));
+
+            w($"Num. Conta: {objContaDoJoao.numero}.");
+            
+            w("Pressione uma tecla para encerrar");
+            r();           
+            
+            objContaDoJoao.Depositar(1000);
+            w($"Saldo do: {objContaDoJoao.correntista.nome} é {objContaDoJoao.saldo}.");
+            r();
+            objContaDoJoao.Sacar(500);
+            w($"Saldo do:{objContaDoJoao.correntista.nome} é {objContaDoJoao.saldo}.");
+            r();           
+            
+        }
+
+       static void w(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+        static void r()
+        {
+            
+            Console.ReadLine();
+        }
+    }
+}
